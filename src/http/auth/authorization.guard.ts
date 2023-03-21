933d680a345e29feb4cd471d8b16a5dc4d2af533
Iago Alexandre <iagoaap@outlook.com>
2023-03-21 06:20:00 -0300

    setup graphql

--- src/http/auth/authorization.guard.ts
+++ src/http/auth/authorization.guard.ts
@@ -5,6 +5,7 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { GqlExecutionContext } from '@nestjs/graphql/dist/services';
 import { expressjwt as jwt, GetVerificationKey } from 'express-jwt';
 import { expressJwtSecret } from 'jwks-rsa';
 import { promisify } from 'node:util';
@@ -22,9 +23,11 @@ export class AuthorizationGuard implements CanActivate {
   }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const httpContext = context.switchToHttp();
-    const req = httpContext.getRequest();
-    const res = httpContext.getResponse();
+    // const httpContext = context.switchToHttp();
+    // const req = httpContext.getRequest();
+    // const res = httpContext.getResponse();
+
+    const { req, res } = GqlExecutionContext.create(context).getContext();
 
     const checkJWT = promisify(
       jwt({
--- src/http/http.module.ts
+++ src/http/http.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from 'src/database/database.module';
-import { TesteController } from 'src/http/teste.controller';
+import { GraphQLModule } from '@nestjs/graphql';
+import path from 'path';
+import { DatabaseModule } from '../database/database.module';
+import { TestResolver } from 'src/http/teste.resolver';
+import { ApolloDriver } from '@nestjs/apollo';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
-  controllers: [TesteController],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+  providers: [TestResolver],
 })
 export class HttpModule {}
