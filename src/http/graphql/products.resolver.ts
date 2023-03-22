4b4f73c18b130b79bbb84e64775346f8f553dbc1
Iago Alexandre <iagoaap@outlook.com>
2023-03-22 06:10:48 -0300

    create product route

--- src/http/graphql/products.resolver.ts
+++ src/http/graphql/products.resolver.ts
@@ -1,8 +1,8 @@
 import { UseGuards } from '@nestjs/common';
-import { Query } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
 import { ProductsService } from 'src/services/products.service';
-import { Resolver } from 'type-graphql';
+import { CreateProductInput } from './inputs/create-product-input';
 import { Product } from './models/Product';
 
 @Resolver()
@@ -13,5 +13,11 @@ export class ProductsResolver {
   // @UseGuards(AuthorizationGuard)
   products() {
     return this.productsService.listAllProducts();
+  }
+
+  @Mutation(() => Product)
+  // @UseGuards(AuthorizationGuard)
+  createProduct(@Args('data') data: CreateProductInput) {
+    return this.productsService.createProduct(data);
   }
 }
--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma/prisma.service';
+import slugify from 'slugify';
+
+interface CreateProductParams {
+  title: string;
+}
 
 @Injectable()
 export class ProductsService {
@@ -7,5 +12,26 @@ export class ProductsService {
 
   async listAllProducts() {
     return this.prisma.product.findMany();
+  }
+
+  async createProduct({ title }: CreateProductParams) {
+    const slug = slugify(title);
+
+    const productWithSameSlug = await this.prisma.product.findUnique({
+      where: {
+        slug,
+      },
+    });
+
+    if (productWithSameSlug) {
+      throw new Error('Another product with same slug already exists');
+    }
+
+    return await this.prisma.product.create({
+      data: {
+        title,
+        slug,
+      },
+    });
   }
 }
